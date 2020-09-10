69ee79ace6fe3507b3520feecdafebdedcbd5cf4
Gouthum Karadi <goukaradi@hotmail.com>
2020-09-10 03:23:04 -0400

    Revised postgres error, ws, and still shpwing calculated Nothing yet

--- client/src/fib.js
+++ client/src/fib.js
@@ -2,76 +2,76 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class Fib extends Component {
-    state = {
-        seenIndexes: [],
-        values: {},
-        index: ''
-    };
+  state = {
+    seenIndexes: [],
+    values: {},
+    index: ''
+  };
 
-    componentDidMount() {
-        this.fetchValues();
-        this.fetchIndexes();
-    }
+  componentDidMount() {
+    this.fetchValues();
+    this.fetchIndexes();
+  }
 
-    async fetchValues() {
-        const values = await axios.get('/api/values/current');
-        this.setState({ values: values.data });
-    }
+  async fetchValues() {
+    const values = await axios.get('/api/values/current');
+    this.setState({ values: values.data });
+  }
 
-    async fetchIndexes() {
-        const seenIndexes = await axios.get('/api/values/all');
-        this.setState({
-            seenIndexes: seenIndexes.data
-        });
-    }
-    // Event handler for submit, helper method.  Bound function.
-    handleSubmit = async (event) => {
-        event.preventDefault();
+  async fetchIndexes() {
+    const seenIndexes = await axios.get('/api/values/all');
+    this.setState({
+      seenIndexes: seenIndexes.data
+    });
+  }
 
-        await axios.post('/api/values', {
-            index: this.state.index
-        });
-        this.setState({ index: '' });
-    }
-    // Note map function and join below iterating over the index array returned from seeIndexes
-    // Postgres by default returns an array of objects, in this case one with the number property and a value
-    renderSeenIndexes() {
-        return this.state.seenIndexes.map(({ number }) => number).join(', ')
-    }
-    // Redis returns an object with key value pairs within it.  
-    // Note how we create a new array with the value for each key
-    renderValues() {
-        const entries = [];
-        for (let key in this.state.values.values) {
-            entries.push(
-                <div key={key}>
-                    For index {key} I calculated {this.state.values[key]}
-                </div>
-            );
-        }
-        return entries;
-    }
+  handleSubmit = async event => {
+    event.preventDefault();
 
-    // Note text of event handler for input field, especially the this.setState for the value
-    render() {
-        return (
-        <div>
-            <form onSubmit={this.handleSubmit}>
-                <label>Enter your index:</label>
-                <input
-                    value={this.setState.index}
-                    onChange={event => this.setState({ index: event.target.value })}
-                />
-                <button>Submit</button>
-            </form>
-            <h3>Indicies I have seen</h3>
-            {this.renderSeenIndexes()}
+    await axios.post('/api/values', {
+      index: this.state.index
+    });
+    this.setState({ index: '' });
+  };
 
-            <h3>Calculated Values:</h3>
-            {this.renderValues()}
+  renderSeenIndexes() {
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
+  }
+
+  renderValues() {
+    const entries = [];
+
+    for (let key in this.state.values) {
+      entries.push(
+        <div key={key}>
+          For index {key} I calculated {this.state.values[key]}
         </div>
-        );
+      );
     }
+
+    return entries;
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Enter your index:</label>
+          <input
+            value={this.state.index}
+            onChange={event => this.setState({ index: event.target.value })}
+          />
+          <button>Submit</button>
+        </form>
+
+        <h3>Indexes I have seen:</h3>
+        {this.renderSeenIndexes()}
+
+        <h3>Calculated Values:</h3>
+        {this.renderValues()}
+      </div>
+    );
+  }
 }
 
 export default Fib;
--- server/index.js
+++ server/index.js
@@ -11,19 +11,15 @@ app.use(bodyParser.json());
 
 // Postgres Client Setup
 const { Pool } = require('pg');
-const pgClient = new Pool({
+const pool = new Pool({
   user: keys.pgUser,
   host: keys.pgHost,
   database: keys.pgDatabase,
   password: keys.pgPassword,
   port: keys.pgPort,
 });
-
-pgClient.on('connect', () => {
-  pgClient
-    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-    .catch((err) => console.log(err));
-});
+ 
+pool.query('CREATE TABLE IF NOT EXISTS values (number INT)')
 
 // Redis Client Setup
 const redis = require('redis');
@@ -41,14 +37,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/values/all', async (req, res) => {
-  // await syntax due to built-in promise support in Postgres library
-  const values = await pgClient.query('SELECT * from values');
+  const values = await pool.query('SELECT * from values');
 
   res.send(values.rows);
 });
 
 app.get('/values/current', async (req, res) => {
-  // callback function no promise support in redis library
   redisClient.hgetall('values', (err, values) => {
     res.send(values);
   });
@@ -63,7 +57,7 @@ app.post('/values', async (req, res) => {
 
   redisClient.hset('values', index, 'Nothing yet!');
   redisPublisher.publish('insert', index);
-  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+  pool.query('INSERT INTO values(number) VALUES($1)', [index]);
 
   res.send({ working: true });
 });
