1648ca06c9757b75506a90228c9342498d04b1e4
combatmonk <gouthum@virtualnex.us>
2020-09-20 11:29:35 -0400

    Empty App.test.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,15 +1,3 @@
-/*
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders other page link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText('Other Page');
-  expect(linkElement).toBeInTheDocument();
-});
-*/
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
